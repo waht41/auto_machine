df6c09ba5f767de288b2990fa55a576018403f70
waht <qwerty@111.com>
2025-02-01 02:26:59 +0800

    添加样例，新建cline provider

---
+++ src/exam/initCline.ts
@@ -0,0 +1,10 @@
+import {ClineProvider} from "@core/webview/ClineProvider";
+import * as vscode from "vscode"
+import {ExtensionContext} from 'vscode'
+import {MockExtensionContext, MockWebviewView} from "vscode";
+const outputChannel = vscode.window.createOutputChannel("Roo-Code")
+const context : vscode.ExtensionContext  = new MockExtensionContext();
+const cp = new ClineProvider(context, outputChannel)
+const webview = new MockWebviewView('mock')
+cp.resolveWebviewView(webview)
+// cp.initClineWithTask('hello')
