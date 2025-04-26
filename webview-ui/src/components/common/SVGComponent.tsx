ed4a63ead0f994834f469d100f4f0134975eedc5
waht <qwerty@111.com>
2025-04-26 21:26:09 +0800

    修复一个严重的性能bug

--- webview-ui/src/components/common/SVGComponent.tsx
+++ webview-ui/src/components/common/SVGComponent.tsx
@@ -21,7 +21,22 @@ interface StyledSVGProps {
   $strokeLinejoin?: 'miter' | 'round' | 'bevel';
   $width?: number | string;
   $height?: number | string;
+  as: React.FC<React.SVGProps<SVGSVGElement>>;
 }
+
+// 创建一个基础的 SVG 容器组件
+const StyledSVGBase = styled.svg<StyledSVGProps>`
+  width: ${props => props.$width}px;
+  height: ${props => props.$height}px;
+  
+  path {
+    ${props => props.$fill && `fill: ${props.$fill} !important`};
+    ${props => props.$stroke && `stroke: ${props.$stroke} !important`};
+    ${props => props.$strokeWidth && `stroke-width: ${props.$strokeWidth} !important`};
+    ${props => props.$strokeLinecap && `stroke-linecap: ${props.$strokeLinecap} !important`};
+    ${props => props.$strokeLinejoin && `stroke-linejoin: ${props.$strokeLinejoin} !important`};
+  }
+`;
 
 const SVGComponent: React.FC<SVGComponentProps> = ({
 	component,
@@ -34,23 +49,10 @@ const SVGComponent: React.FC<SVGComponentProps> = ({
 	strokeLinejoin,
 	...restProps
 }) => {
-	// 创建一个带有样式属性的 styled component
-	const StyledSVG = styled(component)<StyledSVGProps>`
-    width: ${props => props.$width || width}px;
-    height: ${props => props.$height || height}px;
-    
-    path {
-      ${props => props.$fill && `fill: ${props.$fill} !important`};
-      ${props => props.$stroke && `stroke: ${props.$stroke} !important`};
-      ${props => props.$strokeWidth && `stroke-width: ${props.$strokeWidth} !important`};
-      ${props => props.$strokeLinecap && `stroke-linecap: ${props.$strokeLinecap} !important`};
-      ${props => props.$strokeLinejoin && `stroke-linejoin: ${props.$strokeLinejoin} !important`};
-    }
-  `;
-
 	return (
-		<StyledSVG
+		<StyledSVGBase
 			{...restProps}
+			as={component}
 			$fill={fill}
 			$stroke={stroke}
 			$strokeWidth={strokeWidth}
